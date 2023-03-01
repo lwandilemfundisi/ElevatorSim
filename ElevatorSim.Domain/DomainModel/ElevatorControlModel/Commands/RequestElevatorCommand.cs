0c5cda5db953f5897c8f5447e85eea2b72c05658
Lwandile Mfundisi <lwandilemf@tihsa.co.za>
2023-03-01 22:24:37 +0200

    save

---
+++ ElevatorSim.Domain/DomainModel/ElevatorControlModel/Commands/RequestElevatorCommand.cs
@@ -0,0 +1,34 @@
+﻿using Microservice.Framework.Domain.Commands;
+using Microservice.Framework.Domain.ExecutionResults;
+
+namespace ElevatorSim.Domain.DomainModel.ElevatorControlModel.Commands
+{
+    public class RequestElevatorCommand
+        : Command<ElevatorControl, ElevatorControlId>
+    {
+        #region Constructors
+
+        public RequestElevatorCommand(ElevatorControlId aggregateId)
+            : base(aggregateId)
+        {
+        }
+
+        #endregion
+    }
+
+    public class RequestElevatorCommandHandler
+        : CommandHandler<ElevatorControl, ElevatorControlId, RequestElevatorCommand>
+    {
+        #region Virtual Methods
+
+        public override Task<IExecutionResult> ExecuteAsync(
+            ElevatorControl aggregate, 
+            RequestElevatorCommand command, 
+            CancellationToken cancellationToken)
+        {
+            return Task.FromResult(ExecutionResult.Success());
+        }
+
+        #endregion
+    }
+}
