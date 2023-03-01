f64a8c7e543c9608696dbd426968ae5e9a8b955c
Lwandile Mfundisi <lwandilemf@tihsa.co.za>
2023-03-01 22:44:06 +0200

    save

---
+++ ElevatorSim.Domain/DomainModel/ElevatorModel/Commands/RequestElevatorDownCommand.cs
@@ -0,0 +1,35 @@
+﻿using Microservice.Framework.Domain.Commands;
+using Microservice.Framework.Domain.ExecutionResults;
+
+namespace ElevatorSim.Domain.DomainModel.ElevatorModel.Commands
+{
+    public class RequestElevatorDownCommand
+        : Command<Elevator, ElevatorId>
+    {
+        #region Constructors
+
+        public RequestElevatorDownCommand(ElevatorId aggregateId)
+            : base(aggregateId)
+        {
+
+        }
+
+        #endregion
+    }
+
+    public class RequestElevatorDownCommandHandler
+        : CommandHandler<Elevator, ElevatorId, RequestElevatorDownCommand>
+    {
+        #region Virtual Methods
+
+        public override Task<IExecutionResult> ExecuteAsync(
+            Elevator aggregate, 
+            RequestElevatorDownCommand command, 
+            CancellationToken cancellationToken)
+        {
+            return Task.FromResult(ExecutionResult.Success());
+        }
+
+        #endregion
+    }
+}
---
+++ ElevatorSim.Domain/DomainModel/ElevatorModel/Commands/RequestElevatorUpCommand.cs
@@ -0,0 +1,35 @@
+﻿using Microservice.Framework.Domain.Commands;
+using Microservice.Framework.Domain.ExecutionResults;
+
+namespace ElevatorSim.Domain.DomainModel.ElevatorModel.Commands
+{
+    public class RequestElevatorUpCommand
+        : Command<Elevator, ElevatorId>
+    {
+        #region Constructors
+
+        public RequestElevatorUpCommand(ElevatorId aggregateId)
+            : base(aggregateId)
+        {
+
+        }
+
+        #endregion
+    }
+
+    public class RequestElevatorUpCommandHandler
+        : CommandHandler<Elevator, ElevatorId, RequestElevatorUpCommand>
+    {
+        #region Virtual Methods
+
+        public override Task<IExecutionResult> ExecuteAsync(
+            Elevator aggregate,
+            RequestElevatorUpCommand command,
+            CancellationToken cancellationToken)
+        {
+            return Task.FromResult(ExecutionResult.Success());
+        }
+
+        #endregion
+    }
+}
