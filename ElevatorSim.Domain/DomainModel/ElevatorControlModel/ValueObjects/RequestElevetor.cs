3ca8357a35451ed39e7edde14e0d2179ce03a50a
Lwandile Mfundisi <lwandilemf@tihsa.co.za>
2023-03-01 22:27:34 +0200

    save

---
+++ ElevatorSim.Domain/DomainModel/ElevatorControlModel/ValueObjects/RequestElevetor.cs
@@ -0,0 +1,24 @@
+﻿using Microservice.Framework.Common;
+
+namespace ElevatorSim.Domain.DomainModel.ElevatorControlModel.ValueObjects
+{
+    public class RequestElevetor
+        : ValueObject
+    {
+        #region Constructors
+
+        public RequestElevetor(
+            uint floorNumber)
+        {
+            FloorNumber = floorNumber;
+        }
+
+        #endregion
+
+        #region Properties
+
+        public uint FloorNumber { get; }
+
+        #endregion
+    }
+}
