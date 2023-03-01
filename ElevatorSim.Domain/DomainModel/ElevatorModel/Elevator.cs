04828cb09164977406a79a684c71fda4f16f3858
Lwandile Mfundisi <lwandilemf@tihsa.co.za>
2023-03-01 22:39:38 +0200

    save

---
+++ ElevatorSim.Domain/DomainModel/ElevatorModel/Elevator.cs
@@ -0,0 +1,25 @@
+﻿using Microservice.Framework.Domain.Aggregates;
+
+namespace ElevatorSim.Domain.DomainModel.ElevatorModel
+{
+    public class Elevator 
+        : AggregateRoot<Elevator, ElevatorId>
+    {
+        private Action<object, string> LazyLoader { get; set; }
+
+        #region Constructors
+
+        public Elevator()
+            : base(null)
+        {
+        }
+
+        public Elevator(ElevatorId id)
+            : base(id)
+        {
+        }
+
+
+        #endregion
+    }
+}
---
+++ ElevatorSim.Domain/DomainModel/ElevatorModel/ElevatorId.cs
@@ -0,0 +1,20 @@
+﻿using Microservice.Framework.Common;
+using Newtonsoft.Json;
+
+namespace ElevatorSim.Domain.DomainModel.ElevatorModel
+{
+    [JsonConverter(typeof(SingleValueObjectConverter))]
+    public class ElevatorId 
+        : Identity<ElevatorId>
+    {
+        #region Constructors
+
+        public ElevatorId(string value)
+            : base(value)
+        {
+        }
+
+
+        #endregion
+    }
+}
