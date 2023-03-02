72e4a8964ed211a1607396f22667ef3c4bee4dab
Lwandile Mfundisi <lwandilemf@tihsa.co.za>
2023-03-02 23:04:07 +0200

    save

---
+++ ElevatorSim.Tests/UnitTests/ElevatorModel/Specifications/WeightLimitSpecificationTests.cs
@@ -0,0 +1,43 @@
+﻿using ElevatorSim.Domain.DomainModel.ElevatorModel;
+using ElevatorSim.Domain.DomainModel.ElevatorModel.Specifications;
+using ElevatorSim.Tests.Helpers;
+using FluentAssertions;
+
+namespace ElevatorSim.Tests.UnitTests.ElevatorModel.Specifications
+{
+    [Category("Unit")]
+    public class WeightLimitSpecificationTests : Test
+    {
+        [Test]
+        public void Valid()
+        {
+            //Arrange
+            var sut = new WeightLimitSpecification(2);
+            var testAggregate = new Elevator() { Weightlimit = 2};
+
+            //Act
+            var isSatisfiedBy = sut.IsSatisfiedBy(testAggregate);
+            var why = sut.WhyIsNotSatisfiedBy(testAggregate);
+
+            //Assert
+            isSatisfiedBy.Should().BeTrue();
+            why.Should().HaveCount(0);
+        }
+
+        [Test]
+        public void Invalid()
+        {
+            //Arrange
+            var sut = new WeightLimitSpecification(6);
+            var testAggregate = new Elevator() { Weightlimit = 2 };
+
+            //Act
+            var isSatisfiedBy = sut.IsSatisfiedBy(testAggregate);
+            var why = sut.WhyIsNotSatisfiedBy(testAggregate);
+
+            //Assert
+            isSatisfiedBy.Should().BeFalse();
+            why.Should().HaveCount(1);
+        }
+    }
+}
