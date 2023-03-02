21970ae2a8b884abce1295436fbb3404478df287
Lwandile Mfundisi <lwandilemf@tihsa.co.za>
2023-03-02 11:41:17 +0200

    save

---
+++ ElevatorSim.Domain/DomainModel/ElevatorModel/Subscribers/MoveElevatorEventSubscriber.cs
@@ -0,0 +1,39 @@
+﻿using ElevatorSim.Domain.DomainModel.ElevatorControlModel;
+using ElevatorSim.Domain.DomainModel.ElevatorControlModel.Events;
+using Microservice.Framework.Domain.Commands;
+using Microservice.Framework.Domain.Events;
+using Microservice.Framework.Domain.Queries;
+using Microservice.Framework.Domain.Subscribers;
+
+namespace ElevatorSim.Domain.DomainModel.ElevatorModel.Subscribers
+{
+    public class MoveElevatorEventSubscriber
+        : ISubscribeSynchronousTo<ElevatorControl, ElevatorControlId, MoveElevatorEvent>
+    {
+        private readonly ICommandBus _commandBus;
+        private readonly IQueryProcessor _queryProcessor;
+
+        #region Constructors
+
+        public MoveElevatorEventSubscriber(
+            ICommandBus commandBus,
+            IQueryProcessor queryProcessor)
+        {
+            _commandBus = commandBus;
+            _queryProcessor = queryProcessor;
+        }
+
+        #endregion
+
+        #region Virtual Methods
+
+        public Task HandleAsync(
+            IDomainEvent<ElevatorControl, ElevatorControlId, MoveElevatorEvent> domainEvent,
+            CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+
+        #endregion
+    }
+}
