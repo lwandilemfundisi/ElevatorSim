ad60df31a79374a64e30c9ef7efca7682daf916c
Lwandile Mfundisi <lwandilemf@tihsa.co.za>
2023-03-02 20:39:06 +0200

    save

--- ElevatorSim.Persistence/ElevatorControlModelPersistence/Mappings/ElevatorControlModelMapping.cs
+++ ElevatorSim.Persistence/ElevatorControlModelPersistence/Mappings/ElevatorControlModelMapping.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using ElevatorSim.Domain.DomainModel.ElevatorControlModel;
+using ElevatorSim.Domain.DomainModel.ElevatorControlModel.Entities;
+using ElevatorSim.Persistence.ValueObjectConverters;
+using Microsoft.EntityFrameworkCore;
 
 namespace ElevatorSim.Persistence.ElevatorControlModelPersistence.Mappings
 {
@@ -6,6 +9,21 @@ namespace ElevatorSim.Persistence.ElevatorControlModelPersistence.Mappings
     {
         public static ModelBuilder ElevatorControlModelMap(this ModelBuilder modelBuilder)
         {
+            modelBuilder
+                .Entity<ElevatorControl>()
+                .Property(o => o.Id)
+                .HasConversion(new SingleValueObjectIdentityValueConverter<ElevatorControlId>());
+
+            modelBuilder
+                .Entity<ManagedElevator>()
+                .Property(o => o.Id)
+                .HasConversion(new SingleValueObjectIdentityValueConverter<ManagedElevatorId>());
+
+            modelBuilder
+                .Entity<ManagedElevator>()
+                .HasOne<ElevatorControl>()
+                .WithMany(c => c.Elevators);
+
             return modelBuilder;
         }
     }
---
+++ ElevatorSim.Persistence/ElevatorControlModelPersistence/Migrations/20230302183811_Init.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ElevatorSim.Persistence.ElevatorControlModelPersistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class Init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ElevatorControl",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Version = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ElevatorControl", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ManagedElevator",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    ElevatorId = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ElevatorControlId = table.Column<string>(type: "nvarchar(450)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ManagedElevator", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ManagedElevator_ElevatorControl_ElevatorControlId",
+                        column: x => x.ElevatorControlId,
+                        principalTable: "ElevatorControl",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ManagedElevator_ElevatorControlId",
+                table: "ManagedElevator",
+                column: "ElevatorControlId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ManagedElevator");
+
+            migrationBuilder.DropTable(
+                name: "ElevatorControl");
+        }
+    }
+}
