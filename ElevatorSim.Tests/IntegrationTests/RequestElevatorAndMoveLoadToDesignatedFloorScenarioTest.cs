1b0a994d2718b7dad73bdea9ac44b4c70de8633b
Lwandile Mfundisi <lwandilemf@tihsa.co.za>
2023-03-06 18:42:24 +0200

    save

--- ElevatorSim.Tests/IntegrationTests/RequestElevatorAndMoveLoadToDesignatedFloorScenarioTest.cs
+++ ElevatorSim.Tests/IntegrationTests/RequestElevatorAndMoveLoadToDesignatedFloorScenarioTest.cs
@@ -3,6 +3,8 @@ using ElevatorSim.Domain.DomainModel.ElevatorControlModel.Commands;
 using ElevatorSim.Domain.DomainModel.ElevatorControlModel.Entities;
 using ElevatorSim.Domain.DomainModel.ElevatorControlModel.ValueObjects;
 using ElevatorSim.Domain.DomainModel.ElevatorModel;
+using ElevatorSim.Domain.DomainModel.ElevatorModel.Queries;
+using ElevatorSim.Domain.DomainModel.ElevatorModel.ValueObjects.XmlValueObjects;
 using ElevatorSim.Tests.Helpers;
 
 namespace ElevatorSim.Tests.IntegrationTests
@@ -31,6 +33,16 @@ namespace ElevatorSim.Tests.IntegrationTests
                     testElevatorControlId, 
                     new RequestElevetor(1, 2, 3)), CancellationToken.None);
 
+            /*I dont like this test but done it anyway, reason the events happen async so the guarantee
+            of the assert might delay as per machine*/
+
+            //Assert
+            var elevator = await _queryProcessor
+                .ProcessAsync(new GetElevatorQuery(testElevatorId), CancellationToken.None);
+
+            elevator.CurrentFloor.Should().Be(2);
+            elevator.CurrentWeight.Should().Be(0);
+            elevator.ElevatorStatus.Should().Be(ElevatorStatuses.Of().InReady);
         }
     }
 }
