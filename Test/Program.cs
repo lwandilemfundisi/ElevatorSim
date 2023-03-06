67fa780f9a861993f6ff1989c0e193fcfac3b8c5
Lwandile Mfundisi <lwandilemf@tihsa.co.za>
2023-03-06 13:26:46 +0200

    save

---
+++ Test/Program.cs
@@ -0,0 +1,12 @@
+﻿using Test;
+
+var pub = new Publisher();
+var sub1 = new Subscriber("sub1", pub);
+var sub2 = new Subscriber("sub2", pub);
+
+// Call the method that raises the event.
+pub.DoSomething();
+
+// Keep the console window open
+Console.WriteLine("Press any key to continue...");
+Console.ReadLine();
---
+++ Test/Subscriber.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    class Subscriber
+    {
+        private readonly string _id;
+
+        public Subscriber(string id, Publisher pub)
+        {
+            _id = id;
+
+            // Subscribe to the event
+            pub.RaiseCustomEvent += HandleCustomEvent;
+        }
+
+        // Define what actions to take when the event is raised.
+        void HandleCustomEvent(object sender, CustomEventArgs e)
+        {
+            System.Console.WriteLine($"{_id} received this message: {e.Message}");
+        }
+    }
+}
